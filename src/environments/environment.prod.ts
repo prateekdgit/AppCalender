32f54004344bfe1e142d7fc09d941d6e730f4de9
Prateek Darmwal <prateekcsdarmwal@gmail.com>
2021-07-20 14:01:40 +0530

    change live url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'https://artcalendar.sixwebsoft.com/web',
+  apiUrl: 'https://artcalendar.sixwebsoft.com/web/api',
   baseUrl: 'https://artcalendar.sixwebsoft.com',
   assetUrl: 'https://artcalendar.sixwebsoft.com/web',
   // apiUrl: 'mysite.com/api'
